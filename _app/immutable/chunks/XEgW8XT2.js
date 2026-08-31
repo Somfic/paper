9956cbf67159d0134a7a3feaecfc2737edb1cedf
Somfic <lucas@somfic.dev>
2026-08-31 20:57:36 +0000

    Compiled client

---
+++ _app/immutable/chunks/XEgW8XT2.js
@@ -0,0 +1,1 @@
+import{b as p,E as t}from"./DRrKZrvd.js";import{B as c}from"./Ddx4z73m.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
